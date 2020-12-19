135c452ef5fb2c053deb013ca84ddaa0fe53f50b
whsv26 <whsv26@gmail.com>
2020-12-19 21:04:19 +0300

    Move linker and update makefile.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-default:
-	php ./src/linker.php
+PHP80=/usr/bin/php8.0
+
+default: php80
+
+php80:
+	${PHP80} ./src/linker.php
--- src/linker.php
+++ src/linker.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\parser;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Process\Process;
 
